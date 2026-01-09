6169f5e6887e3cefa4340d181f91b45a28a337c7
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 14:51:38 +0530

    commited by n8n

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,3 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from backend.config.settings import Config
 
 db = SQLAlchemy()
-
-def create_app() -> Flask:
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app)
-    return app
