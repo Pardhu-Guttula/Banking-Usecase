70a9780fcd1a8de2417bc07e479e7230cbcd04f3
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 15:00:12 +0530

    committed by n8n

---
+++ app.py
@@ -0,0 +1,10 @@
+from backend import create_app, db
+from backend.controllers.auth import auth_bp
+
+app = create_app()
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run()
