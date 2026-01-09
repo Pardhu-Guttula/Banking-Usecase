472922d8b6cff7cc0cb6832340a8c28bcc7a4568
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 14:51:33 +0530

    commited by n8n

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from backend.config.settings import Config
-from backend import db
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
     return app
