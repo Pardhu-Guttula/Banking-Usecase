d5bc4d44e99857d176792453e8e3a9e540ef2ad9
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 14:51:29 +0530

    commited by n8n

--- backend/repositories/auth/__init__.py
+++ backend/repositories/auth/__init__.py
@@ -1,10 +1,9 @@
 from backend.models.auth import User
+from backend import db
+
+def add_user(user: User) -> None:
+    db.session.add(user)
+    db.session.commit()
 
 def get_user_by_email(email: str) -> User:
     return User.query.filter_by(email=email).first()
-
-def create_user(email: str, password: str) -> User:
-    user = User(email=email, password=password)
-    db.session.add(user)
-    db.session.commit()
-    return user
