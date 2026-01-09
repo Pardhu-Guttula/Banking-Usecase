b67bc115c70cec0d65be25f0de8be4de9c2d098d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 15:01:20 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from backend import create_app, db
 from backend.controllers.auth import auth_bp
+from backend.controllers.profiles import profiles_bp
 
 app = create_app()
 app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(profiles_bp, url_prefix='/profiles')
 
 if __name__ == '__main__':
     with app.app_context():
