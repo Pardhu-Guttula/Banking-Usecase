d5172ab699c9a0e639700bd36313d2718b54c3db
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 14:50:23 +0530

    commited by n8n

--- backend/models/auth/__init__.py
+++ backend/models/auth/__init__.py
@@ -1,6 +1,6 @@
 from backend import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
